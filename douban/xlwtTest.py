7373b6c79e553fd8940953bab65f04c107d55874
kevin zheng <6987096-zhengkw@users.noreply.gitlab.com>
2021-11-04 15:37:50 +0800

    新增demo，操作excel,写一个99乘法表

--- douban/xlwtTest.py
+++ douban/xlwtTest.py
@@ -12,4 +12,18 @@ import xlwt
 workbook =xlwt.Workbook(encoding='utf-8')
 #获取sheet对象
 worksheet=workbook.add_sheet('sheet1')
-worksheet.write()
+worksheet.write(0,0,"第一个单元格内容") #arg1行，arg2列，arg3 写入内容
+#定义一个新sheet，打印99乘法表，一个单元格对应一个计算公式+结果
+sheet=workbook.add_sheet('99乘法表')
+
+for i in range(0,9):
+    i=i+1
+    for j in range(0,9):
+        if i == j: break
+        j=j+1
+    #     print(str(i)+'*'+str(j)+'='+str(i*j),end=" ")
+    # print('\n')
+        #写数据
+        sheet.write(i-1,j-1,str(i)+'*'+str(j)+'='+str(i*j))
+workbook.save('test.xls')
+
