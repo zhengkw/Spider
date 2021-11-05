ba213b3a39dec602de433c852ca2ef7d1aa281dc
kevin zheng <6987096-zhengkw@users.noreply.gitlab.com>
2021-11-05 14:09:31 +0800

    字符串 join方法练习

---
+++ demo3/StringDemo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @CreateTime   : 21/11/05 下午 1:56
+# @Author       : kevin
+# @File         : StringDemo.py
+# @Software     : PyCharm
+# @UpdateTime   : 21/11/05 下午 1:56
+# @Version      : v1.0.0
+
+#join一个list
+lst=['hello','java','Python','Scala']
+print(','.join(lst))
+#join一个元组
+tuple=('hello','java','Python','Scala')
+print('|'.join(tuple))
+#join单字符串
+print('*'.join('Java'))
